880eded14648ed8827dde3d7b1805b5673f0b6be
lukePeavey <1williampeavey@gmail.com>
2017-10-01 22:42:24 -0400

    Setup the express app

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+const authentication = require('./config/authentication')
+
+const DATABASE_URL = process.env.DATABASE_URL
+
+// Initialize the app
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+
+// Setup API routes
+app.use(routes.users)
+app.use(routes.auth)
+app.use(routes.weather)
+app.use(routes.places)
+
+// Initialize database connection
+mongoose.Promise = global.Promise
+mongoose.connect(DATABASE_URL, { useMongoClient: true })
+
+console.log(process.env.NODE_ENV)
+
+module.exports = app

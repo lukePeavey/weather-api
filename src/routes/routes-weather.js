eb005ac26c76b6c698da4e386f6e420502ff6d0a
lukePeavey <1williampeavey@gmail.com>
2017-10-01 23:41:16 -0400

    Add weather API routes

--- src/routes/routes-weather.js
+++ src/routes/routes-weather.js
@@ -1,11 +1,23 @@
 const router = require('express').Router()
-
+const fetch = require('node-fetch')
+const API_BASE = 'http://api.wunderground.com/api'
+const KEY = process.env.WEATHER_UNDERGROUND_KEY
 /**
- * @param {Array} features - See API docs for supported features
- * @param {string} place - US zip code or longitude,latitude
- *
- * @see https://www.wunderground.com/weather/api/d/docs
+ * @param {Array} features
+ * @param {string} place - the location for the weather request.
+ *   This can be a US zip code or longitude,latitude
  */
-router.get('/weather/', async (req, res, next) => {})
+router.get('/weather/', async (req, res, next) => {
+  const {query, params} = req
+  let features = String(query.features).split(',').join('/')
+  let place = query.place || 'autoip'
+  try {
+    const apiResponse = await fetch(`${API_BASE}/${KEY}/${features}/q/${place}.json`)
+    const data = await apiResponse.json()
+    res.json(data)
+  } catch(err) {
+    res.status
+  }
+})
 
 module.exports = router

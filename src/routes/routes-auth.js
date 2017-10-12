8bd8e28299ec49183eb11e8fe5619fd4477c3da0
Luke Peavey <1williampeavey@gmail.com>
2017-10-12 04:30:58 -0400

    Update authentication routes

--- src/routes/routes-auth.js
+++ src/routes/routes-auth.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const { tokenExpiration } = require('../config/authentication')
+const { pick } = require('lodash')
 
-const authentication = {
-  local: passport.authenticate('local', { session: false })
-}
+/** Authenticates request using local authentication strategy */
+const authenticate = passport.authenticate('local', { session: false })
 
 /**
  * Login Route
@@ -13,42 +14,39 @@ const authentication = {
  * username and password, a json web token (JWT) is sent to the client which
  * can be used to authenticate subsequent requests to the API.
  */
-router.post('/auth/login', authentication.local, async (req, res, next) => {
+router.post('/auth/login', authenticate, async (req, res, next) => {
   const user = req.user
-  const payload = {
-    fullName: user.fullName,
-    email: user.email,
-    id: user.id
-  }
-  const token = jwt.sign(payload, process.env.SECRET_KEY)
-  // @todo - handle successful login
-  res.json({ message: 'LOGIN SUCCESSFUL', token })
+  const payload = pick(user, ['fullName', 'email', 'id'])
+  const token = jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: tokenExpiration
+  })
+  res.json({ status: 'OK', token, tokenExpiration })
 })
 
 /**
  * Registration Route
  * Creates a new user account with the provided data.
+ * @todo Handle successful registration (?)
+ * @todo handle errors (?)
  */
 router.post('/auth/register', async (req, res, next) => {
-  // Registration form data
+  // Extract form data
   const { firstName, lastName, email, password } = req.body
-  const name = { first: firstName, last: lastName }
 
   // Make sure all of the required fields are present
   if (!firstName || !lastName || !email || !password) {
-    return res.status(422).send('INVALID REQUEST')
+    return res.status(401).json({ status: 'MISSING REQUIRED FIELDS' })
   }
 
   try {
     // Check if the username exists already
     let user = await User.findOne({ email })
     if (user !== null) {
-      res.status(422).send('A USER WITH THAT EMAIL ADDRESS ALREADY EXISTS')
+      res.status(422).json({ status: 'EMAIL ADDRESS ALREADY REGISTERED' })
     } else {
       // Create a new user and save it to the database
-      user = await new User({ email, password, name }).save()
-      // @todo - handle successful registration
-      res.status(200).json({ message: 'REGISTRATION SUCCESSFUL', user })
+      user = await new User({ email, password, firstName, lastName }).save()
+      res.status(200).json({ status: 'OK', user })
     }
   } catch (err) {
     console.error(err)

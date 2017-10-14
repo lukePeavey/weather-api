75b65794d9cb62e4de4d87b93229fb2e646ebc93
Luke Peavey <1williampeavey@gmail.com>
2017-10-13 23:37:36 -0400

    Update server configuration
    - Enable cross origin requests from a specified domain (if the client-side app is not deployed on the same domain as the API server).
    
    - The Allow-credentials header was required in order to include  cookies in fetch requests from the client ( with credentials: 'include').

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const authentication = require('./config/authentication')
 const cookieParser = require('cookie-parser')
 
 // Environment variables
-const { SECRET_KEY, DATABASE_URL } = process.env
+const { SECRET_KEY, DATABASE_URL, DOMAIN } = process.env
 
 // Initialize the app
 const app = express()
@@ -16,6 +16,15 @@ app.use(bodyParser.json())
 app.use(cookieParser(SECRET_KEY))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(passport.initialize())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', DOMAIN )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Cache'
+  )
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 
 // Setup API routes
 app.use(routes.users)

9f8566bd6d8d65caa51f70274975705a7a007164
Luke Peavey <1williampeavey@gmail.com>
2017-10-31 22:41:49 -0400

    Update weather API routes
    - Added individual routes for each of the weather API endpoints that are used in the app.
    
    - Added some server side normalization of api data.

--- src/routes/routes-weather.js
+++ src/routes/routes-weather.js
@@ -1,22 +1,101 @@
 const router = require('express').Router()
 const fetch = require('node-fetch')
+const { get, isEmpty } = require('lodash')
+const createError = require('http-errors')
+const normalizeWeather = require('../utils/normalizeWeather')
 const API_BASE = 'http://api.wunderground.com/api'
 const KEY = process.env.WEATHER_UNDERGROUND_KEY
+
 /**
- * @param {Array} features
- * @param {string} place - the location for the weather request.
- *   This can be a US zip code or longitude,latitude
+ * Makes a request to the weather underground API.
+ * The requeset can include any of the supported API features, such as
+ * 'conditions', 'forecast10day', etc. At least one feature must be specified.
+ * If the place is not specified, it will default to 'autoip', which detects
+ * the user's approximate location based on IP.
+ *
+ * @param {string} req.query.place - longitude,latitude | zip code | default is 'autoip'
+ * @param {string} req.query.features - comma separated list of weather api feaetures to include
+ * @see {@link https://www.wunderground.com/weather/api/d/docs}
  */
-router.get('/weather/', async (req, res, next) => {
-  const { query, params } = req
-  let features = String(query.features)
+router.get('/weather', async (req, res, next) => {
+  const { query } = req
+  const place = query.place || 'autoip'
+  const features = String(query.features)
     .split(',')
     .join('/')
-  let place = query.place || 'autoip'
+
   try {
     const apiResponse = await fetch(`${API_BASE}/${KEY}/${features}/q/${place}.json`)
-    const data = await apiResponse.json()
-    res.json(data)
+    const { response, ...data } = await apiResponse.json()
+    if (isEmpty(data)) {
+      return next(createError(422, get(response, 'error.description', 'Unable to get weather')))
+    }
+    res.status(200).json(normalizeWeather(data))
+  } catch (err) {
+    return next(err)
+  }
+})
+
+/**
+ * Gets the current weather conditions
+ * If place is not specified, it will default to 'autoip', which detects the
+ * user's approximate location based on ip.
+ *
+ * @param {string} req.query.place - longitude,latitude | zip code (default is autoip)
+ * @see {@link https://www.wunderground.com/weather/api/d/docs?d=data/conditions}
+ */
+router.get('/weather/current/:place?', async (req, res, next) => {
+  const { query, params } = req
+  const place = params.place || query.place || 'autoip'
+  try {
+    const apiResponse = await fetch(`${API_BASE}/${KEY}/conditions/q/${place}.json`)
+    const { response, ...data } = await apiResponse.json()
+    if (isEmpty(data)) {
+      return next(createError(422, get(response, 'error.description', 'Unable to get weather')))
+    }
+    res.status(200).json(normalizeWeather(data))
+  } catch (err) {
+    return next(err)
+  }
+})
+
+/**
+ * Gets 10 days of daily weather forecast data.
+ *
+ * @param req.query.place - longitude,latitude | zip code (default is autoip)
+ * @see {@link https://www.wunderground.com/weather/api/d/docs?d=data/forecast10day}
+ */
+router.get('/weather/days/:place?', async (req, res, next) => {
+  const { query, params } = req
+  const place = params.place || query.place || 'autoip'
+  try {
+    const apiResponse = await fetch(`${API_BASE}/${KEY}/forecast10day/q/${place}.json`)
+    const { response, ...data } = await apiResponse.json()
+    if (isEmpty(data)) {
+      return next(createError(422, get(response, 'error.description', 'Unable to get weather')))
+    }
+    res.status(200).json(normalizeWeather(data))
+  } catch (err) {
+    return next(err)
+  }
+})
+
+/**
+ * Get 10 days of hourly weather forecast data.
+ *
+ * @param {string} req.query.place - longitude,latitude | zip code (default is autoip)
+ * @see {@link https://www.wunderground.com/weather/api/d/docs?d=data/hourly10day}
+ */
+router.get('/weather/hours/:place?', async (req, res, next) => {
+  const { query, params } = req
+  const place = params.place || query.place || 'autoip'
+  try {
+    const apiResponse = await fetch(`${API_BASE}/${KEY}/hourly10day/q/${place}.json`)
+    const { response, ...data } = await apiResponse.json()
+    if (isEmpty(data)) {
+      return next(createError(422, get(response, 'error.description', 'Unable to get weather')))
+    }
+    res.status(200).json(normalizeWeather(data))
   } catch (err) {
     return next(err)
   }
---
+++ src/utils/normalizeWeather.js
@@ -0,0 +1,54 @@
+const { pickBy, identity, get, mapKeys } = require('lodash')
+
+function normalizeUnit(value, key) {
+  return key === 'fahrenheit' ? 'english' : key === 'celsius' ? 'metric' : key
+}
+
+/**
+ * Cleans up the data returned by the API.
+ * Reduces unnecessary nesting, fixes some inconsistent property name, and
+ * returns the data in more logical structure that makes client side code
+ * cleaner and easier to read.
+ *
+ * @todo - This should be handled on the client using library like normalizr.
+ * @todo - Add documention on the returned data structure and how it differs
+ * from the data returned directly from WU API.
+ */
+module.exports = function normalizeWeather(data) {
+  let days = get(data, 'forecast.simpleforecast.forecastday')
+  let current = get(data, 'current_observation')
+  let hours = get(data, 'hourly_forecast')
+
+  if (current) {
+    let { display_location, observation_location, image, ...conditions } = current
+    current = { location: display_location, conditions }
+  }
+
+  if (days) {
+    let forecastDayNightText = data.forecast.txt_forecast.forecastday
+    days = days.map((forecastDay, i) => {
+      const { high, low, ...conditions } = forecastDay
+      const dayText = forecastDayNightText[i * 2]
+      const nightText = forecastDayNightText[i * 2 + 1]
+      const summary = {
+        day: { english: dayText.fcttext, metric: dayText.fcttext_metric },
+        night: { english: nightText.fcttext, metric: nightText.fcttext_metric }
+      }
+      return {
+        ...conditions,
+        summary,
+        high: mapKeys(high, normalizeUnit),
+        low: mapKeys(low, normalizeUnit)
+      }
+    })
+  }
+
+  if (hours) {
+    hours = hours.map(forecastHour => {
+      const { FCTTIME: date, ...properties } = forecastHour
+      return { ...properties, date }
+    })
+  }
+
+  return pickBy({ current, days, hours }, identity)
+}
